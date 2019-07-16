60b39ddd585943dbdb39cca1910b9fffa063db45
Phillip Bell <phillipabell@gmail.com>
2019-07-16 08:24:05 -0600

    HTTP REST GET using flassk

---
+++ README.md
@@ -0,0 +1,23 @@
+# Python HTTP services and Test Automation library
+
+Creates a HTTP RESTful service and automates testing of that service.
+
+## Installation
+
+Clone the files to your local repository.
+
+Requires Python 3.7
+Flask
+
+## Usage
+
+python rest.py # To run a local HTTP server 
+
+curl http://127.0.0.1:5000/us_state_codes/Colorado # returns 'words'
+foobar.pluralize('goose') # returns {
+    "us-state": "Colorado",
+    "us-state-code": "CO"
+}
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ restpkg/rest.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+us_state_codes =  [
+    {
+    "us-state": "Colorado",
+    "us-state-code": "CO"
+    },
+    {
+    "us-state": "Florida",
+    "us-state-code": "FL"
+    },
+    {
+    "us-state": "New York",
+    "us-state-code": "NY"
+    }
+]
+
+class US_State_Codes(Resource):
+
+    def get(self, name):
+        for us_state in us_state_codes:
+           if (name == us_state["us-state"]):
+               return us_state, 200
+        return "No US state found", 404
+
+api.add_resource(US_State_Codes, "/us_state_codes/<string:name>")
+
+app.run(debug=True)
